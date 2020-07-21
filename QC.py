4b9fc4133a5ef1a988a9e22f60257937dbd6d76b
Dana Bar Ilan <44768525+danab9@users.noreply.github.com>
2020-07-21 10:18:44 +0300

    added -i and -o

--- QC.py
+++ QC.py
@@ -38,7 +38,7 @@ def validate_input(line_dict):
     return
 
 
-def trim(flags_file, path=''):
+def trim(flags_file, path='fastq/raw/', prefix_out="fastq/trimmed/"):
     """
     construct the trimmomatic command and execute it
     :param flags_file: csv input file
@@ -48,8 +48,6 @@ def trim(flags_file, path=''):
     if not os.path.exists("fastq/trimmed/"):
         os.mkdir("fastq/trimmed/")
 
-    prefix_fq = path + "fastq/raw/"
-    prefix_out = path + "fastq/trimmed/"
 
     with open(flags_file, 'r') as csv_file:
         file = csv.DictReader(csv_file)  # reads csv as dict. header is keys. each line is a dict.
@@ -61,8 +59,8 @@ def trim(flags_file, path=''):
 
             if ends == 'PE':  # paired ends
                 # create input and output files
-                in_f = prefix_fq + line['input_forward']
-                in_r = prefix_fq + line['input_reverse']
+                in_f = path + line['input_forward']
+                in_r = path + line['input_reverse']
                 out_f = prefix_out + line['input_forward'].rstrip('.fastq.gz')
                 out_f_paired = out_f + '_paired.fastq.gz'
                 out_f_unpaired = out_f + '_unpaired.fastq.gz'
@@ -73,7 +71,7 @@ def trim(flags_file, path=''):
                 command += [in_f, in_r, out_f_paired, out_f_unpaired, out_r_paired, out_r_unpaired]
             else:  # single ends
                 # create input and output files
-                inp = prefix_fq + line['input_forward']
+                inp = path + line['input_forward']
                 out = prefix_out + line['input_forward'].rstrip('.fastq.gz')
                 out_paired = out + '_paired.fastq.gz'
                 out_unpaired = out + '_unpaired.fastq.gz'
@@ -99,7 +97,7 @@ def trim(flags_file, path=''):
 
 
 # produce a template csv file used in trimming
-def template_csv(fq_path):
+def template_csv(fq_path='fastq/raw/', out_loc='template.csv'):
     """
     create template file with default arguments for trimmomatic
     :param path: path for all fastq.gz files.
@@ -107,7 +105,7 @@ def template_csv(fq_path):
 
     fq_R1 = fnmatch.filter(os.listdir(fq_path), "*R1*.fastq.gz")  # Assuming reads are marked as "R1" and "R2"!!
 
-    with open('template.csv', 'w') as output:
+    with open(out_loc, 'w') as output:
         headers = ['ends', 'input_forward', 'input_reverse', 'phred', 'threads', 'ILLUMINACLIP:', 'SLIDINGWINDOW:',
                    'LEADING:', 'TRAILING:', 'CROP:', 'HEADCROP:', 'MINLEN:']
         writer = csv.DictWriter(output, fieldnames=headers)
@@ -122,22 +120,22 @@ def template_csv(fq_path):
                 'TRAILING:': '28',
             })
 
-    print('finished template file. check it out at template.csv in your working directory.')
+    print('finished template file. find it here: %s' %out_loc)
 
 
-def fastqc_reports(out_dir, working_dir='fastq/raw/'):
+def fastqc_reports(out_dir="QC/", in_dir='fastq/raw/'):
     out_dest = out_dir + "fastqc/"
     os.mkdir(out_dest)
-    for fqfile in os.listdir(working_dir):
+    for fqfile in os.listdir(in_dir):
         if not fqfile.endswith(".fastq.gz"):
             continue  # step over files that are not fastq.gz format
-        print(working_dir+fqfile)
+        print(in_dir + fqfile)
         with open("error.log", 'w') as log:
-            subprocess.call(['fastqc', working_dir+fqfile, "--outdir=%s" % out_dest], stderr=log, stdout=log)
+            subprocess.call(['fastqc', in_dir + fqfile, "--outdir=%s" % out_dest], stderr=log, stdout=log)
     print('Finished fastqc reports. Find them here: %s' % out_dest)
 
 
-def multiqc_report(out_dir):
+def multiqc_report(out_dir="QC/"):
     in_dir = out_dir + 'fastqc/'
     print('Start multiqc report')
     subprocess.call(['multiqc', in_dir, '-o', out_dir])
@@ -145,53 +143,71 @@ def multiqc_report(out_dir):
 
 
 if __name__ == '__main__':
+
     parser = argparse.ArgumentParser()
     group = parser.add_mutually_exclusive_group()
-    group.add_argument("-t", "--trim", help="QC trimming. Provide csv file with trimmomatic flags as columns. ",
+    group.add_argument("-t", "--trim", help="QC trimming. Provide csv file with trimmomatic flags as columns ",
                        type=str, metavar="[CSV]", nargs=1)
-    group.add_argument("--template", help="Produce trimmomatic auto-trimmig template csv file with default values",
-                       dest='template_fqpath', const="fastq/raw/", nargs="?")
-    group.add_argument("-r", "--reports",  help="Produce fastqc and multifastqc reports of all fastq.gz files in input"
-                                                "directory. Reports output path is optional, default is QC/fastqc/.",
-                       type=str, dest='reports_outdir', nargs="?", const='QC/fastqc/')
-    parser.add_argument("-w", "--working_dir", help="Working directory of the program. Default is current directory",
+    group.add_argument("--template", help="produce trimmomatic auto-trimmig template csv file with default values",
+                       action="store_true")
+    group.add_argument("-r", "--reports",  help="produce fastqc and multifastqc reports of all fastq.gz files in input"
+                                                "directory \nIf output path not provided (-o), default is QC/fastqc/",
+                       action="store_true")
+    parser.add_argument("-i", help="working directory of the program. \noptional, default is current directory",
                         nargs=1, type=str, dest='wd')
+    parser.add_argument("-o", help="output path - optional.", nargs=1, type=str, dest='output_path')
 
     args = parser.parse_args()
 
     if args.wd:
-        wd = args.wd
+        wd = args.wd[0]
     else:
-        wd = os.getcwd()
+        wd = ""
+
+    out_path = ""
+    if args.output_path:
+        out_path = args.output_path
+        if not out_path.endswith('/'):
+            out_path += '/'
 
     if args.trim:
         print('trim')
         print(args.trim[0])
-        if args.wd:
-            print(args.wd[0])
-            trim(args.trim[0], args.wd[0])
+        if wd and out_path:
+            trim(args.trim[0], path=wd, prefix_out=out_path)
+            print('finished trimming. results are found in %s provided' % out_path)
+        elif wd or out_path:
+            if wd:
+                trim(args.trim[0], path=wd)
+                print('finished trimming. results are found in fastq/trimmed directory')
+            else:
+                trim(args.trim[0], prefix_out=out_path)
+                print('finished trimming. results are found in %s provided' % out_path)
         else:
-            print("not args wd")
             trim(args.trim[0])
-        print('finished trimming. results are found in fastq/trimmed directory')
+            print('finished trimming. results are found in fastq/trimmed directory')
 
-    elif args.reports_outdir:  # reports
+    elif args.reports:  # reports
         print('reports')
-        outdir=args.reports_outdir
-        if not outdir.endswith('/'):
-            outdir += '/'
-        fastqc_reports(outdir)
-        multiqc_report(outdir)  # at the moment: multiqc produces report in outdir
+        if out_path:
+            fastqc_reports(out_path)
+            multiqc_report(out_path)
+        else:
+            fastqc_reports()
+            multiqc_report()
 
-    elif args.template_fqpath:
-        if args.wd:
-            print('No need to specify working directory when creating template. Exiting.')
-            exit(1)
+    elif args.template:
+        if wd and out_path:
+            template_csv(fq_path=wd, out_loc=out_path)
+        elif wd or out_path:
+            if wd:
+                template_csv(fq_path=wd)
+            else:
+                template_csv(out_loc=out_path)
+        else:  # use defaults only
+            template_csv()
         # TODO: add PE SE options to create the right template
         # TODO: check SE as well
-        print('producing template file')
-        print(args.template_fqpath)
-        template_csv(args.template_fqpath)
 
     else:  # if user did not choose an option -> print help
-        parser.print_usage()
+        parser.print_help()
