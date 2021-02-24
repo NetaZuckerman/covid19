ea58383063171628d122fcce7423bd3c57a943a6
Dana Bar Ilan <44768525+danab9@users.noreply.github.com>
2021-02-24 11:33:53 +0200

    added excel highlighted Amino Acid mutations table to pipeline and corrected region of WHO mutations in novelMutTable.csv

--- pipeline.sh
+++ pipeline.sh
@@ -197,9 +197,9 @@ function muttable() {
     conda deactivate
 
     conda activate CoronaPipeline
-    python /data/projects/Dana/scripts/covid19/MutTable.py alignment/all_aligned.fasta results/muttable.xlsx
+    python /data/projects/Dana/scripts/covid19/MutTable.py alignment/all_aligned.fasta results/nuc_muttable.xlsx
+    python /data/projects/Dana/scripts/covid19/MutTable.py alignment/all_aligned.fasta results/AA_muttable.xlsx /data/projects/Dana/scripts/covid19/regions.csv
     python /data/projects/Dana/scripts/covid19/variants_softcode.py alignment/all_aligned.fasta results/variants.csv results/pangolinClades.csv
-
 
 #    mkdir -p BAM/readcounts
 #    for file in BAM/*.mapped.sorted.bam; do
--- translated_table.py
+++ translated_table.py
@@ -3,12 +3,30 @@ from Bio import SeqIO, Seq, AlignIO
 import pandas as pd
 from Bio.Seq import translate
 from Bio.Alphabet import Gapped, generic_dna, IUPAC
+# import xlsxwriter
 import Bio
 
 # user input:
 aligned_fasta_path = argv[1]
 outfile_path = argv[2]
 regions_table_path = argv[3]
+
+
+def highlight_row(row):
+    colors_list = [""] * 7 + ["background-color: grey"] * 2
+    mut = row["mut"]
+    ref = row["REF"]
+    for samp in row[9:]:
+        if samp != ref:
+            if samp == mut:
+                color = "background-color: yellow"
+            else:
+                color = "background-color: teal" if samp == 'X' else "background-color: green"
+        else:
+            color = ''
+        colors_list.append(color)
+    return colors_list
+
 
 codon_map = {
     "TTT": "F", "TTC": "F", "TTA": "L", "TTG": "L",
@@ -56,19 +74,30 @@ def full_codon_gaps(sequence, start, end, gap='-'):
 
 def translate(sequence, start, end, codon_table):
     """
-    translate nucleotides sequence to amino acid sequence according to codons in region start-end
-    :param sequence: nucleotides sequence (Seq? or str?) # TODO decide
+    translate nucleotides sequence in given region to amino acid sequence according to codons in region start-end
+    :param sequence: nucleotides sequence as str #
     :param start: position of first nucleotide
     :param end: position of last nucleotide
     :return: translated sequence (aa seq)
     """
+    tranlsated = []
+    for i in range(start, end, 3):
+        codon = sequence[i:i+3]
+        if codon in codon_table:
+            aa = codon_table[codon]
+        else:
+            aa = 'X' # ignore frameshift
+        tranlsated.append(aa)
+    return tranlsated
 
 
 # 1. load sequences and tables
 regionsTable = pd.read_csv(regions_table_path)
 multifasta = SeqIO.to_dict(SeqIO.parse(aligned_fasta_path, 'fasta'))
+multifasta.pop('NC_045512.2', None)
+multifasta.pop('REF_NC_045512.2', None)
 
-mutTable = pd.read_csv("novelMutTable.csv")  # TODO: change to server's path
+mutTable = pd.read_csv("/data/projects/Dana/scripts/covid19/novelMutTable.csv")
 # 2. keep only non-synonymous mutations
 mutTable = mutTable[mutTable.type == 'non -Synonymous ']
 finalTable = mutTable
@@ -76,26 +105,30 @@ finalTable = mutTable
 for sample, record in multifasta.items():
     seq = record.seq
     sample_muts = []
-    print(sample)
-    try:
-        for mut in mutTable.iterrows():
-            pos = mut[1][5]
-            gene = mut[1][2]
-            aa = mut[1][1]
-            aa_number = int(aa[1:-1])  # strip letters of both sides
-            aa_from = aa[0]
-            aa_to = aa[-1]
-            region_start = int(regionsTable[regionsTable.id == gene].start.values[0])
-            region_end = int(regionsTable[regionsTable.id == gene].end.values[0])
-            new_seq = full_codon_gaps(seq, region_start, region_end)
-
-            region_aa = new_seq[region_start-1:region_end].translate(gap='-')
-    except Bio.Data.CodonTable.TranslationError:
-        print('sample: {} - Unable to translate'.format(sample))
-        # add to list?
+    for mut in mutTable.iterrows():
+        pos = mut[1][5]
+        gene = mut[1][2]
+        aa = mut[1][1]
+        aa_number = int(aa[1:-1])  # strip letters of both sides
+        aa_from = aa[0]
+        aa_to = aa[-1]
+        region_start = int(regionsTable[regionsTable.id == gene].start.values[0])
+        region_end = int(regionsTable[regionsTable.id == gene].end.values[0])
+        region_translated = translate(str(seq), region_start-1, region_end, codon_map)
+        alt = region_translated[aa_number-1]
+        sample_muts.append(alt)
+    finalTable[sample] = sample_muts
 
 
+varcol = finalTable.apply(lambda row: row[8:].unique(), axis=1)
+finalTable.insert(6, 'var', varcol)
+finalTable = finalTable.sort_values(by=["gene", "lineage"], ascending=[False, True])
+finalTable = finalTable.rename(columns={'pos': 'nuc pos', 'nucleotide': 'nuc name', 'AA': 'name'})
 
-# # TODO: TO FIX -> A-- not translated. maybe translate with my own function??
-# # finalTable.REF = finalTable.REF.str.upper()
-#
+sorted_cols = ['nuc pos', 'nuc name', 'type', 'gene', 'var', 'name', 'lineage', 'REF', 'mut']
+finalTable = finalTable[sorted_cols + [col for col in finalTable.columns if col not in sorted_cols]]
+
+# write to file
+finalTable["REF"] = finalTable.apply(lambda row: row["name"][0], axis=1)
+finalTable["mut"] = finalTable.apply(lambda row: row["name"][-1], axis=1)
+finalTable.style.apply(lambda row: highlight_row(row), axis=1).to_excel(outfile_path, index=False)
