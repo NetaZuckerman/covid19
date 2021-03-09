58e014f25603aba1b1a88cf4eba7f05ef0655443
Dana Bar Ilan <44768525+danab9@users.noreply.github.com>
2021-03-09 10:25:15 +0200

    corrected set location (non covered muts)

--- variants_softcode.py
+++ variants_softcode.py
@@ -141,7 +141,7 @@ for sample, sample_mutlist in samples_mutations.items():
     unexpected_mutations = specific_cases(unexpected_mutations, sample, known_variant)
 
     more_muts = set(more_muts)
-    samples_not_covered = set(samples_not_covered)
+
     # if not suspect and (more_muts or samples_s_not_covered[sample] or unexpected_mutations[sample]):
     if not suspect and (more_muts or samples_not_covered[sample] or unexpected_mutations[sample]):
         suspect = 'suspect'
@@ -154,6 +154,7 @@ for sample, sample_mutlist in samples_mutations.items():
         pangolin_status = ''
         pangolin_note = ''
     QCfail = True if pangolin_status == 'fail' else False
+    samples_not_covered = set(samples_not_covered)
     line = {
         "Sample": sample,
         "Known Variant": known_variant if known_variant and not QCfail else 'no variant',
