bced0995362215135450ec8ce22432a195df63ca
Dana Bar Ilan <44768525+danab9@users.noreply.github.com>
2021-05-09 13:42:53 +0300

    added documentation + new field pulled from nextclade table

--- variants.py
+++ variants.py
@@ -65,14 +65,20 @@ alignment.pop('NC_045512.2', None)
 alignment.pop('REF_NC_045512.2', None)
 
 # prepare nextclade dataframe
-clades_df = clades_df[['seqName', 'aaSubstitutions', 'clade', 'deletions']]  # or aaDeletions instead?? # TODO
+clades_df = clades_df[['seqName', 'aaSubstitutions', 'aaDeletions', 'clade']]  # or aaDeletions instead?? # TODO
 clades_df = clades_df.rename(columns={'seqName': 'sample'})
 clades_df['sample'] = clades_df['sample'].apply(str)
 clades_df = clades_df.fillna('')
+# create dict of aasubstitutions and aadeletions. key=sample id, value=list of substitutions..
 aa_substitution_dict = {}
-for sample in clades_df['sample']:
-    aasubs = clades_df[clades_df['sample']==sample].aaSubstitutions.values.tolist()
-    aa_substitution_dict[sample] = [f"{x.split(':')[1]}({x.split(':')[0]})" for x in aasubs[0].split(',')] if (aasubs and aasubs != ['']) else ''
+aa_deletions_dict = {}
+for sample in clades_df['sample']: # for each sample aggregate
+    aasubs = clades_df[clades_df['sample'] == sample].aaSubstitutions.values.tolist()
+    aadels = clades_df[clades_df['sample'] == sample].aaDeletions.values.tolist()
+    aa_substitution_dict[sample] = [f"{x.split(':')[1]}({x.split(':')[0]})" for x in aasubs[0].split(',')] \
+        if (aasubs and aasubs != ['']) else ''
+    aa_deletions_dict[sample] = [f"{x.split(':')[1]}({x.split(':')[0]})" for x in aadels[0].split(',')] \
+        if (aadels and aadels != ['']) else ''
 
 # some variables:
 samples_mutations = {id: [] for id in alignment}
@@ -115,56 +121,60 @@ for sample, sample_mutlist in samples_mutations.items():
     suspect = ''
     lin_percentages = {}
     lin_number = {}
-    # iterate over mutations of each lineage
+    # iterate over mutations of each lineage, check how much of the lineage's mutations are covered by sample -
+    # to decide on variant
     for lin, linmuts in mutations_by_lineage.items():
         temp = [x for x in linmuts]
         temp_mutes = []
-        for mut in sample_mutlist:
-            if mut in temp:
+        for mut in sample_mutlist:  # iterate over  table mutations of sample
+            if mut in temp:  # remove mutations of sample from temp list and keep in temp_mutes list
                 temp.remove(mut)
                 temp_mutes.append(mut)
-        if not temp:
-            # all mutations of that lineage exists.
+        if not temp:  # all mutations of that lineage exists in sample -> known variant=lineage name
             known_variant = lin
-        elif len(linmuts) != len(temp):  # some mutations do exist
-            more_muts += temp_mutes
+        elif len(linmuts) != len(temp):  # some of lineage's mutations exist but not all
+            more_muts += temp_mutes  # keep found mutation in more_mutes
+            # calculate percentage of lineage mutations found:
             lin_percentages[lin] = round(len(set(temp_mutes)) / len(set(linmuts)) * 100, 2)
-            lin_number[lin] = (len(set(temp_mutes)), len(set(linmuts)))  # tuple: (#lin_mutation_sample, #tot_lin_mutations)
+            lin_number[lin] = (len(set(temp_mutes)), len(set(linmuts)))  # tuple:
+            # (#lin_mutation_sample, #tot_lin_mutations)
 
-    if known_variant:
+    if known_variant:  # remove from more_mutes the mutations of the known variant to show only additional mutations
         more_muts = [x for x in more_muts if x not in mutations_by_lineage[known_variant]]
-    else:  # did not find variant that has all mutations in sample
-        max=0
+    else:  # did not find variant that has 100% mutations in sample
+        max = 0
         var = ''
-        flag = False
+        flag = False  # True means known variant (at least 60% mutations)
         val = 0
         fraction = 0
-        for key, tup in lin_number.items():
-            val = round((tup[0] / tup[1])*100, 2)
-            frac = f'({int(tup[0])}/{int(tup[1])})'
-            if val >= 60:  # 60%
+        for key, tup in lin_number.items():  # pick variant that has highest muations % but at least 60 %
+            # val = round((tup[0] / tup[1])*100, 2)
+            val = lin_percentages[key]
+            frac = f'({int(tup[0])}/{int(tup[1])})'  # keep as fraction to print to table
+            if val >= 60:  # 60% -> knwon variant
                 flag = True
-            if val > max:
+            if val > max:  # make sure to choose best match and not the first over 60%
                 max = val
                 var = key
                 fraction = frac
-        if flag:
+        if flag:  # found >60%
             known_variant = var
+            # keep only additional mutations and not lineage mutations of known var.
             more_muts = [x for x in more_muts if x not in mutations_by_lineage[known_variant]]
-        elif val >= 50 and var:  # and < 60% because no known variant
+        elif val >= 50 and var:  # > 50% mutations and < 60% because no known variant
             more_muts = [x for x in more_muts if x not in mutations_by_lineage[var]]
 
-        if var and lin_number[var][0] >= 2:  # At least 2 mutations of lineage
-            # suspect = 'suspect_' + var + ": " + str(lin_percentages[var]) + "%"
+        if var and lin_number[var][0] >= 2:  # At least 2 mutations of lineage --> suspect variant
             suspect = f'suspect_{var}: {str(lin_percentages[var])}{fraction}'  # suspect_<lineagename>): (%)(x/y)
     unexpected_mutations = specific_cases(unexpected_mutations, sample, known_variant)
 
-    more_muts = set(more_muts)  # lose redundancies
+    more_muts = set(more_muts)  # lose redundancies of mutations
 
     if not suspect and (more_muts or samples_not_covered[sample] or unexpected_mutations[sample]):
+        # not specific suspect variant but some mutations exist \ not covered in sequencing - write as suspect
         suspect = 'suspect'
 
-    # get pangolin info
+    # get pangolin info from table
     try:
         pangolin_clade = pangolinTable[pangolinTable.taxon == sample].lineage.values[0]
         pangolin_status = pangolinTable[pangolinTable.taxon == sample].status.values[0]
@@ -175,6 +185,7 @@ for sample, sample_mutlist in samples_mutations.items():
         pangolin_note = ''
     QCfail = True if pangolin_status == 'fail' else False
 
+    # specific cases of mutations in the same location:
     temp = unexpected_mutations[sample].copy()
     for x in temp:
         short_name = x.split('(')[0]
@@ -190,12 +201,12 @@ for sample, sample_mutlist in samples_mutations.items():
             if 'Q677H' in more_muts:
                 unexpected_mutations[sample].remove(x)
 
-    # aa_substitution_list = clades_df[clades_df['sample'] == sample].aaSubstitutions
-
+    # get nextclade info from table
     nextclade = clades_df[clades_df['sample'] == sample].clade
+    # create final table's line (sample line)
     line = {
         "Sample": sample,
-        "Known Variant": known_variant if known_variant and not QCfail else 'no variant',
+        "Known Variant": known_variant if known_variant and not QCfail else 'no monitored variant',
         "Suspect": suspect,
         "More Mutations": ';'.join(set([x + "(" + mutTable[mutTable.AA == x].gene.values[0] + ")" for x in more_muts])),
         # "S Not Covered": ';'.join(samples_s_not_covered[sample]),
