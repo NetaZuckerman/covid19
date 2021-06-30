5673f62bf4f31e2ded0c63834bbb824880b4f641
Dana Bar Ilan <44768525+danab9@users.noreply.github.com>
2021-06-30 07:17:16 +0300

    print empty rows to log file

--- MutTable.py
+++ MutTable.py
@@ -44,8 +44,7 @@ fastadict.pop('REF_NC_045512.2', None)
 
 samples = []  # list to aggregate a samples column to.
 # iterate over fasta records of aligned fasta file and get values in mutations positions
-df = df.dropna(thresh=6)
-df.to_csv('concat_table.csv')
+df = df.dropna(thresh=3)
 for file, seqrecord in fastadict.items():
     seq = seqrecord.seq
     samples.append(file)  # keep sample names in list
